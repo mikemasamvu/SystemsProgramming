752345053a1ba47c84378532ebf29a5590ba07fa
mam462 <mike.masamvu@yale.edu>
2024-07-03 01:49:36 -0400

    Update

--- final-proj/.deps/p-allocator.d
+++
@@ -1,7 +0,0 @@
-obj/p-allocator.o: uspace/p-allocator.c shared/lib.h uspace/malloc.h \
- uspace/process.h shared/x86-64.h shared/lib.h
-shared/lib.h:
-uspace/malloc.h:
-uspace/process.h:
-shared/x86-64.h:
-shared/lib.h:
--- final-proj/.deps/p-malloc.d
+++
@@ -1,7 +0,0 @@
-obj/p-malloc.o: uspace/p-malloc.c shared/lib.h uspace/malloc.h \
- uspace/process.h shared/x86-64.h shared/lib.h
-shared/lib.h:
-uspace/malloc.h:
-uspace/process.h:
-shared/x86-64.h:
-shared/lib.h:
